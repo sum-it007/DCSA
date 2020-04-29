cb99fceef611896d05c63ea8aa0f4cb0fe86458e
Sarv1997 <singhsarvpreet14@gmail.com>
2020-04-29 18:59:33 +0530

    6th section complete

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Student Developer Connector</h1>
+          <p className="lead">
+            Department of Computer Science and Application (DCSA)
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
